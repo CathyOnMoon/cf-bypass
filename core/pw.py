fc5ed84ef8de82679db552b299066a51d9f11b3c
CathyOnMoon <cathyonmoon@gmail.com>
2025-05-13 19:13:07 +0800

    更改

--- core/pw.py
+++ core/pw.py
@@ -7,24 +7,36 @@ if __name__ == '__main__':
         # 启动浏览器，设置代理
         proxy_settings = ProxySettings({
             "server": "http://superproxy.zenrows.com:1337",  # 代理地址和端口
-            "username": "7Mh7Hyrdx3Hb",  # 若需要认证
-            "password": "KTwhyriwVqLm9kd_region-ap"  # 若需要认证
+            "username": "7Mh7Hyrdx3Hb",
+            "password": "D6D7EKLnhe6gC6T_region-ap"
         })
         browser = p.chromium.launch(
-            # proxy=proxy_settings,
+            proxy=proxy_settings,
             headless=False,
             args=[
                 "--disable-blink-features=AutomationControlled",  # 隐藏自动化标识
+                "-no-first-run",
+                "-force-color-profile=srgb",
+                "-metrics-recording-only",
+                "-password-store=basic",
+                "-use-mock-keychain",
+                "-export-tagged-pdf",
+                "-no-default-browser-check",
+                "-disable-background-mode",
+                "-enable-features=NetworkService,NetworkServiceInProcess,LoadCryptoTokenExtension,PermuteTLSExtensions",
+                "-disable-features=FlashDeprecationWarning,EnablePasswordsAccountStorage",
+                "-deny-permission-prompts",
+                "-disable-gpu",
             ]
         )
         context = browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
         )
         # 创建新页面并跳转
-        page = browser.new_page()
+        page = context.new_page()
         page.goto("https://gmgn.ai/api/v1/gas_price/sol", timeout=60000)
-
-        time.sleep(60)
+        page.wait_for_timeout(5000)
+        time.sleep(30)
 
         # 关闭浏览器
         browser.close()
