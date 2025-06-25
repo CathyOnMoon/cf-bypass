170fbf53c0b902b44f62f6f576335371e407d868
CathyOnMoon <cathyonmoon@gmail.com>
2025-06-25 22:32:55 +0800

    fix

--- server/server.py
+++ server/server.py
@@ -59,7 +59,7 @@ class HttpServer:
                 })
             headers = {
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
                 'Accept-Language': 'en-US,en;q=0.9',
                 'Cache-Control': 'max-age=0',
                 'Cookie': proxy_cookie.cookies,
